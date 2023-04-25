ead3e177198b18f04ced892dd18462a4e85eca23
SirGram <119138378SirGram@users.noreply.github.com>
2023-04-25 19:40:59 +0200

    Ex 1, 2, 3, 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const charList= [];
+let string = "";
+const reverseString = function(sentence) {
+    for (let i =0;i<=sentence.length;i++){
+        const char = sentence[i];
+        charList.push(char);
 
+    }
+    charList.reverse();
+    string=charList.join("");
+    return string;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elements) {
+    let i=0;
+    while(i< array.length){
+        if (elements.includes(array[i])){
+            array.splice(i,1);
+        }
+        else i++;
 
+    }
+    console.log(array);
+    return array;
 };
+removeFromArray([23,6,32,8,8],[32]);
 
 // Do not edit below this line
 module.exports = removeFromArray;
