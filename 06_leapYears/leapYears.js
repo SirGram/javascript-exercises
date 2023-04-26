0e213ab854f86f2a42f00a7d6b96f15490a97760
SirGram <119138378SirGram@users.noreply.github.com>
2023-04-26 17:18:05 +0200

    Ex 5

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if ((year % 100 === 0 && year % 400 != 0) || (year %4 != 0)){
+        console.log("f");
+        return false;
+    }
+    else {
+        console.log("t");
+        return true;
+    }
 
 };
+leapYears(400);
 
 // Do not edit below this line
 module.exports = leapYears;
